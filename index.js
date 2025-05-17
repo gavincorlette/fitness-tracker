92b238665ee09669ff833196efd9e8cc3ed185e6
Gavin Corlette <gavinccorlette@gmail.com>
2025-05-17 09:09:26 -0400

    set up server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express');
-
 const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server is live!");
+});
+
+app.listen(PORT, () => console.log("App is listening on port " + PORT));

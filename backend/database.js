08f0e23202d1efea7b1d63be6f655cde396bab69
Gavin Corlette <gavinccorlette@gmail.com>
2025-05-25 10:51:27 -0400

    connected postgreSQL docker container to backend

---
+++ backend/database.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'fitness_user',
+  host: 'localhost',
+  database: 'fitness_db',
+  password: 'fitness_password',
+  port: 5432,
+});
+
+module.exports = pool;
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const fs = require('fs');
+const db = require('./database');
 
 const workouts = [];
 
